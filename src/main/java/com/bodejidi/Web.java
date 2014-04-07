44e6d3608affe6955a57ab76a0f4737fc09df33a
xushuangshuang <xushuangshuang1@gmail.com>
2014-04-07 16:37:45 +0800

    实现了在页面登录成功后打印出一句话

--- src/main/java/com/bodejidi/Web.java
+++ src/main/java/com/bodejidi/Web.java
@@ -1,5 +1,6 @@
 package com.bodejidi;
 
+import java.io.PrintWriter;
 import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +14,24 @@ public class Web extends HttpServlet
 	}
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
 	{
-		resp.getWriter().println("接受");
+		 PrintWriter out = resp.getWriter();
+		 resp.setContentType("text/html");
+		 String firstName = req.getParameter("firstName");
+		 String lastName = req.getParameter("lastName");
+		 String username = req.getParameter("username");
+		 String password = req.getParameter("password");
+		 String repassword = req.getParameter("repassword");
+		 String email = req.getParameter("email");
+		 String phone = req.getParameter("phone");
+		 String address = req.getParameter("address");
+
+                 out.println("<html>");
+                 out.println("  <head>");
+                 out.println("    <title>Login Success</title>");
+                 out.println("  </head>");
+                 out.println("  <body>");
+                 out.println("    <h1>Welcome Admin!</h1>");
+                 out.println("  </body>");
+                 out.println("</html>");
 	}
 }
